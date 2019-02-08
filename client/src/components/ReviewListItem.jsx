aea17acb5d4a5708fa72847c8a7141036c19ac51
tyler bailey <baile320@umn.edu>
2019-02-07 21:36:04 -0600

    Straggler form previous commit

--- client/src/components/ReviewListItem.jsx
+++ client/src/components/ReviewListItem.jsx
@@ -1,27 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const ReviewListItem = (props) => {
+  var options = { month: 'short', day: 'numeric', year: 'numeric' };
+  let date = new Date(props.review.publishDate).toLocaleDateString("en-US", options)
+
+  let reaction = props.review.score > 60 ? "positive" : props.review.score > 40 ? "mixed" : "negative";
 
   return (
     <div className="review pad_top2 pad_btm2">
       <div className="head_wrap">
         <div className="score_wrap">
-          <div className="metascore_w header_size movie positive indiv">Score</div>
+          <div className={`metascore_w header_size movie ${reaction} indiv`}>{props.review.score.toFixed(0)}</div>
         </div>
         <div className="pub_wrap title">
           <span className="source">
-            <a href="#">Publication</a>
+            <a href="#">{props.review.publication}</a>
           </span>
-          <span className="dash">-</span>
+          <span className="dash"> &ndash; </span>
           <span className="author">
-            <a href="#">Author</a>
+            <a href="#">{props.review.author}</a>
           </span>
-          <div className="date pad_top_half">Date</div>
+          <div className="date pad_top_half">{date}</div>
         </div>
       </div>
       <div className="summary_wrap pad_top_half">
         <div className="summary">
-          <a href="#" className="no_hover">Body</a>
+          <a href="#" className="no_hover">{props.review.body}</a>
+          <br />
           <a href="#" className="read_full">Read Full Review</a>
         </div>
       </div>
@@ -30,3 +36,7 @@ const ReviewListItem = (props) => {
 };
 
 export default ReviewListItem;
+
+ReviewListItem.propTypes = {
+  review: PropTypes.object
+};
