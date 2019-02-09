b0ada819e64155c5164529fa45d2d0defc2d2d96
tyler bailey <baile320@umn.edu>
2019-02-09 14:14:07 -0600

    Fix bug found by TravisCI

--- test/App.test.jsx
+++ test/App.test.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import _ from 'underscore';
 
 import App from '../client/src/App';
 import Metascore from '../client/src/components/Metascore';
 import ReviewList from '../client/src/components/ReviewList';
-// eslint-disable-next-line
-import queryDataMock from './mocks/queryData.mock.js';
+import reviews from '../examples/reviews';
+
 
 describe('App Unit Tests', () => {
+  // mock queryData
+  jest
+    .spyOn(App.prototype, 'queryData')
+    // eslint-disable-next-line func-names
+    .mockImplementation(function () {
+      const testReviews = _.sortBy(reviews, 'score').reverse();
+      this.setState({ reviews: testReviews });
+
+      let scores = reviews.map(review => review.score);
+      scores = _.sortBy(scores);
+      this.setState({ scores });
+    });
+
   const wrapper = shallow(<App />);
 
   it('Should render a Metascore component', () => {
