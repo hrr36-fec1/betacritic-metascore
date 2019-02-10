c83e7fb10bc9f3956d7c755920657747771ff260
tyler bailey <baile320@umn.edu>
2019-02-10 14:14:52 -0600

    Refactor to use process.env ports or default to 4444

--- database/index.js
+++ database/index.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoUri = 'mongodb://localhost/metascore';
-
+const mongoUri = process.env.MONGODB_URI || 'mongodb://localhost/metascore';
 const db = mongoose.connect(mongoUri, { useNewUrlParser: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.log(err));
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const PORT = 4444;
+const PORT = process.env.PORT || 4444;
 
 app.listen(PORT, () => {
   console.log(`Connected to Express on port ${PORT}`);
