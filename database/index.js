b09ed5f66e09038402163b6217ef6ab6c0ab6501
tyler bailey <baile320@umn.edu>
2019-02-18 23:41:37 -0600

    Add code so tests will run in a test env

--- database/index.js
+++ database/index.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Review = require('./Review.js');
 const Movie = require('./Movie.js');
 
-const mongoHostName = process.env.NODE_ENV === 'testing' ? 'test1' : 'test2';
-const mongoUri = process.env.MONGODB_URI || 'mongodb://mongo:27017/metascore';
+const mongoHostName = process.env.NODE_ENV === 'testing' ? 'localhost' : 'mongo';
+const mongoUri = process.env.MONGODB_URI || `mongodb://${mongoHostName}:27017/metascore`;
 console.log('------------------DATABASE--------------------------');
 const db = mongoose.connect(mongoUri, { useNewUrlParser: true })
   .then(() => console.log(`Connected to MongoDB on: ${mongoUri}`))
